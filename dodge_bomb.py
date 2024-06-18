061a57fc4c39880a3e9632f9254b747c62be1690
akiguru <c0a23160@edu.teu.ac.jp>
2024-06-18 17:25:39 +0900

    演習課題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,6 +13,17 @@ DELTA = {  # 移動量辞書
     pg.K_RIGHT: (+5, 0),
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+ALFA = {#向き転換辞書
+    (0, 0): pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0), #初期状態 
+    (0, -5): pg.transform.rotozoom(pg.transform.flip(pg.image.load("fig/3.png"),True, False), 90, 2.0), #右斜め上
+    (+5, -5): pg.transform.rotozoom(pg.transform.flip(pg.image.load("fig/3.png"),True, False), 45, 2.0), #右
+    (+5, 0):  pg.transform.rotozoom(pg.transform.flip(pg.image.load("fig/3.png"), True, False), 0, 2.0), #右斜め下
+    (+5, +5): pg.transform.rotozoom(pg.transform.flip(pg.image.load("fig/3.png"), True, False), -45, 2.0), #下
+    (0, +5): pg.transform.rotozoom(pg.transform.flip(pg.image.load("fig/3.png"),True, False), 270, 2.0), #
+    (-5, +5): pg.transform.rotozoom(pg.image.load("fig/3.png"), 45, 2.0),
+    (-5, 0): pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0),
+    (-5, -5): pg.transform.rotozoom(pg.image.load("fig/3.png"), -45, 2.0),
+    }
 
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
@@ -67,21 +78,21 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
-    bb_img = pg.Surface((20, 20))  # 1辺が20の空のSurfaceを作る
+    bb_img = pg.Surface((20, 20))
     bb_img.set_colorkey((0, 0, 0))
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 空のSurfaceに赤い円を描く
-    bb_rct = bb_img.get_rect()  # 爆弾Rect
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) #1辺が20の空のSurfaceを作る
+    bb_rct = kk_img.get_rect() #　爆弾Rect
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-    vx, vy = +5, +5  # 爆弾の横方向速度，縦方向速度
+    vx, vy = +5, +5 # 爆弾の横方向速度、縦方向速度
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        if kk_rct.colliderect(bb_rct):  # 衝突判定
+        if kk_rct.colliderect(bb_rct):#衝突判定
             gameOver(screen)
-            return  # ゲームオーバー
+            return #ゲームオーバー
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -93,18 +104,22 @@ def main():
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        screen.blit(kk_img, kk_rct)
+
+        dx = ALFA[tuple(sum_mv)] #方向転換辞書それぞれの値をdxに与える
+        screen.blit(dx, kk_rct)
+
 
         bb_rct.move_ip(vx, vy)
         yoko, tate = check_bound(bb_rct)
-        if not yoko:  # 横方向にはみ出たら
+        if not yoko:  #横にはみ出たら
             vx *= -1
-        if not tate:  # 縦方向にはみ出たら
+        if not tate:  #縦にはみ出たら
             vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
+
 
 
 if __name__ == "__main__":
